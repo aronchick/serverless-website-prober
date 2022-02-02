f0e8b2f635cd0164c2af3d0d15ec13845ff862a1
David Aronchick <aronchick@gmail.com>
2022-02-02 00:31:24 +0000

    removing a number of redundant events

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,30 +3,30 @@ from operator import contains
 from unittest.mock import patch
 import sys
 
-import urllib3
+import requests
 from src.util.http import is_url_valid
 
 
 class MockHTTPReturnCode(object):
     def __init__(self, status_code) -> None:
-        self.status = 200
+        self.status_code = 200
 
 
-@patch("urllib3.PoolManager.urlopen")
+@patch("requests.head")
 def test_is_url_valid_good(mock_urlopen):
     mock_urlopen.return_value = MockHTTPReturnCode(200)
 
     assert is_url_valid("MOCK_VALID_URL")
 
 
-@patch("urllib3.PoolManager")
+@patch("requests.head")
 def test_is_url_valid_bad(mock_poolmanager):
     error_string = "MOCK_INVALID_URL is invalid."
-    mock_poolmanager.side_effect = urllib3.exceptions.MaxRetryError("", "", error_string)
+    mock_poolmanager.side_effect = requests.exceptions.ConnectionError("", "", error_string)
 
     try:
         is_url_valid("MOCK_INVALID_URL")
 
         assert False  # Should have raised invalid URL error
-    except urllib3.exceptions.MaxRetryError as e:
-        assert "MOCK_INVALID_URL is invalid." in e.reason
+    except requests.exceptions.ConnectionError as e:
+        assert "MOCK_INVALID_URL is invalid." in str(e)
